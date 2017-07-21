9cbab9bb239e418ef83889946d70bad1961264d2
Alexey Sarapulov <aesee@mail.ru>
2017-07-21 18:31:27 +0500

    Add logic to replys

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Python_server
-Simple server-client application
+# Python_gdbserver
+
+
+Server-client stub-application
+
+What's new:
+
+** v0.2 21/07/2017 **
+Connecting with GDB and replying something on almost all query.
--- server.py
+++ server.py
@@ -1,23 +1,39 @@
 # -- coding: cp1251 --
-
 import socket
 
-switch = { 1:'PacketSize=119',
-           2:'',
-           3:'OK',
-           4:'', #'T0;tnotrun:0',
-           5:'S05',
-           6:'m 0',
-           7:'l',#'1000',
-           8:'OK',
-           9:'QC-1',#"QC '0'",
-           10:'1',
-           11:'Text=0;Data=0;Bss=0', #'xxxxxxxx00000000xxxxxxxx00000000',#'Text=0333e564;Data=0;Bss=0333e564;',
-           12:'xxxxxxxx00000000xxxxxxxx00000000',
-           13:'00',
-           14:'m 0',
-           15:'l',
-           16:'stub' }
+switch = { '$qSupported':'swbreak+;PacketSize=131072', #119?
+           '$vMustReplyEmpty':'',
+           '$Hg0':'OK',
+           #'$Hg0':'',
+           '$Hg-1':'Ok',
+           #'$qTStatus':'T1',
+           '$qTStatus':'',
+           #'+$S05':'S05',
+           '$qfThreadInfo':'m 0',
+           #'$qfThreadInfo':'',
+           '$qsThreadInfo':'l',
+           '$Hc-1':'OK',
+           #'$Hc-1':'',
+           '$qC':'',
+           '$qAttached':'1',
+           #'$qAttached':'',
+           '$qOffsets':'Text=00;Data=00;Bss=0',
+           #'$qOffsets':'',
+           #'$g#67':'xxxxxxxx00000000xxxxxxxx00000000',
+           '$g#67':'00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000', 
+           #'$p20':'00',
+           '$p20':'0010000000000000',
+           '$qSymbol':'',
+           '$vKill':'Ok',
+           '+$?':'S00',
+           #'$?':'S05',
+           '$qTfV':'',
+           '$qTsP':'',
+           #'$qL12':'',
+           '$qL12':'0x0000000000001000 in ?? ()',
+           'vCont?':'vCont;c',
+           #'$m':'12345',
+            }
 
 def Checksum(data):
     checksum = 0
@@ -26,12 +42,54 @@ def Checksum(data):
     #return hex(checksum)[-2:]
     return checksum & 0xff
 
-#пока что data не используется
-def Message(i):
-    #return "OK"
-    #return "PacketSize=3ff;qXfer:features:read+"
-    #return "qfThreadInfo"
-    return switch[i]
+def ReadMemory(data):
+    return "2f86"
+
+def ReadRegisters(data):
+    return "123456789abcdef0"
+
+def WriteRegisters(data):
+    return "Ok"
+
+def WriteRegisterN(data):
+    return "Ok"
+
+def WriteMemory(data):
+    return "Ok"
+
+def LastSignal(data):
+    return "S00"
+
+def Step():
+    return LastSignal("OK")
+
+def Continue():
+    return LastSignal("OK")
+
+def Message(data):
+    if (data.find('$m') != -1):
+        return ReadMemory(data)
+    elif (data.find('$g') != -1):
+        return ReadRegisters(data)
+    elif (data.find('$G') != -1):
+        return WriteRegisters(data)
+    elif (data.find('$P') != -1):
+        return WriteRegisterN(data)
+    elif (data.find('$M') != -1):
+        return WriteMemory(data)
+    elif (data.find('$?') != -1):
+        return LastSignal(data)
+    elif (data.find('$s') != -1):
+        return Step() 
+    elif (data.find('$c') != -1):
+        return Continue()
+    else:
+        for key in switch:
+#        if data.startswith(key,2):
+            if (data.find(key) != -1):
+                return switch[key]
+    return 'Ok'
+#    return switch[i]
 
 class GDBClientHandler(object):
     def __init__(self, clientsocket):
@@ -50,40 +108,27 @@ class GDBClientHandler(object):
         print("connected:", addr)
         log = open("log.txt", 'w')
         msg, cs = "",""
-        i = 0
         data = ''
         pastData = ''
         ##получаем от клиента по 1кб информации в цикл,
         ##пока клиент не закончит слать информацию
         while True:
-            #while (data=="b''"):
-            data = str(conn.recv(1024))
+            pastData=data
+            while (data==pastData):
+                data = str(conn.recv(1024).decode('cp1251'))
             log.write(data)
             log.write("\n")
             print("Received: ", data)
-            i+=1
-            msg=Message(i)
+            msg=Message(data)
             cs=Checksum(msg)
             try:
-                #if (data!="b''"):
                 self.send(msg)
-                ##tosend="+$"+str(msg)+"#"+str(cs)
-                ##conn.send(bytes(tosend, encoding='utf-8'))
+                print("Sending: ", msg)
             except Exception:
                 print("Client is not answering.")
             log.write("+$"+str(msg)+"#"+str(cs)[-2:])
             log.write("\n")
-            if (i > 15):
-                while True:
-                    pastData=data
-                    while (data==pastData):                    
-                        data = str(conn.recv(1024))
-                    log.write(data)
-                    log.write("\n")
-                    print("Received: ", data)
-                    msg="OK"
-                    cs=Checksum(msg)
-                    self.send(msg)
+
         ##закрываем соединение
         print("Bye!")
         self.netout.close()
@@ -95,9 +140,12 @@ class GDBClientHandler(object):
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ##устанавливаем связь с клиентом
-#port = 9090
+#port = 3333
 #sock.bind(('',port))
 sock.bind(('',3333)) ##связываем сокет с хостом и портом
 sock.listen(1) ## максимальное количество подключений в очереди
 conn, addr = sock.accept() ##принимаем новый сокет и адрес клиента
 GDBClientHandler(conn).run()
+
+#############################################################
+#def qL12():   
